f2d3fdf305a1ded2aa2964d2421d72a48ad0c34d
ngrishanov <nikita@padsplit.com>
2020-07-14 15:50:51 +0500

    Added submission model to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Submission
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    readonly_fields = ['internal_id', 'function_code', 'status']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
